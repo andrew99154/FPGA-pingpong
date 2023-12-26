58e4a85c84a23a72bcd2d862e0d188aecf04d3ee
jammywangyihan <72907855+jammywangyihan@users.noreply.github.com>
2023-12-26 17:51:12 +0800

    Update process_next_state.v

--- process_next_state.v
+++ process_next_state.v
@@ -11,57 +11,67 @@ module process_next_state(reset,p1l,p1r,p2l,p2r,ball_x,ball_y,time_cnt,game_stat
 	output reg [1:0] game_next_state;
 	output reg [3:0] p1_score;
 	output reg [3:0] p2_score;
+	
+	parameter p1_serve =2'd0;
+	parameter p2_serve =2'd1;
+	parameter playing =2'd2;
+	parameter game_end =2'd3;
+	parameter goal_points =3'd7;
+	parameter game_times = 6'd60;
+	parameter p1_board_x= 10'd150;
+	parameter p2_board_x= 10'd490;
+	
 always @(posedge clk or negedge reset)
 begin
 	if(!reset)
 	begin
-		game_next_state=2'd0;//reset game to p1_serve
+		game_next_state=p1_serve;//reset game to p1_serve
 		p1_score=3'd0;
 		p2_score=3'd0;
 	end
 	else
 	begin
-		if(game_state == 2'd0) //p1_serve
+		if(game_state == p1_serve) //p1_serve
 		begin
-			if(p2_score>=3'd7)
+			if(p2_score>=goal_points)
 			begin
-				game_next_state=2'd3;
+				game_next_state=game_end;
 			end
-			else if(p1l== 1'd0 || p1r == 1'd0) //p1 start the ball
-				game_next_state=2'd2;
+			else if(p1l== 1'd1 || p1r == 1'd1) //p1 start the ball
+				game_next_state=playing;
 			else //keep p1_sreve
-				game_next_state=2'd0;
+				game_next_state=p1_serve;
 		end
-		else if(game_state == 2'd1)//p2_serve
+		else if(game_state == p2_serve)//p2_serve
 		begin
-			if(p1_score>=7)
+			if(p1_score>=goal_points)
 			begin
-				game_next_state=2'd3;
+				game_next_state=game_end;
 			end
-			if(p2l == 1'd0 || p2r == 1'd0) //p2 start the ball
-				game_next_state=2'd2;
+			if(p2l == 1'd1 || p2r == 1'd1) //p2 start the ball
+				game_next_state=playing;
 			else //keep_p2_serve
-				game_next_state=2'd1;
+				game_next_state=p2_serve;
 		end
-		else if(game_state == 2'd2)//playing
+		else if(game_state == playing)//playing
 			begin
-			if(ball_x>10'd490)//p1 goal ,from playing to p2_serve
+			if(ball_x>p2_board_x)//p1 goal ,from playing to p2_serve
 			begin
-				game_next_state = 2'd1;
+				game_next_state = p2_serve;
 				p1_score=p1_score+1'd1;
 			end 
-			else if(ball_x<10'd150)//p2 goal ,from playing to p1_reserve
+			else if(ball_x<p1_board_x)//p2 goal ,from playing to p1_reserve
 			begin
-				game_next_state=2'd0;
+				game_next_state=p1_serve;
 				p2_score=p2_score+1'd1;
 			end
-			else if(time_cnt>=5'd60)//times out
-				game_next_state=2'd3;
+			else if(time_cnt>=game_times)//times out
+				game_next_state=game_end;
 			else//keep playing
-				game_next_state=2'd2;
+				game_next_state=playing;
 			end
 		else//end 2'd3 keep end
-			game_next_state=2'd3;
+			game_next_state=game_end;
 	end
 end
 	
